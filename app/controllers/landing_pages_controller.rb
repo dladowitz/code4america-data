b847b2e48d9b50fcf6b7df27f55b62adcb01a0ee
David Ladowitz <david@ladowitz.com>
2016-06-04 13:34:36 -0700

    calculating violation statistics

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,6 +1,18 @@
 class LandingPagesController < ApplicationController
 
   def landing
+    @category_stats = {}
+    find_category_stats
     render layout: "guest_pages/guest_layout"
+  end
+end
+
+def find_category_stats
+  Violation.all.each do |violation|
+    if @category_stats[violation.category]
+      @category_stats[violation.category] += 1
+    else
+      @category_stats[violation.category] = 1
+    end
   end
 end
